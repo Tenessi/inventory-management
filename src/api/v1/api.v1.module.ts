f0e8a69bada75eb4320d2cf011b28325611116ee
Tenessi <krotov.sergey.017@mail.ru>
2025-03-11 16:48:09 +0600

    added product module

--- src/api/v1/api.v1.module.ts
+++ src/api/v1/api.v1.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
 
-const modules = [UserModule, AuthModule];
+const modules = [UserModule, AuthModule, ProductModule];
 
 @Module({
   imports: modules,
